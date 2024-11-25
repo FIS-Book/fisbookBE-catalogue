cef04f3a4bc499ae1e5c59ad56be62d76d85548f
Sandra Jaramillo Dorado <186032883+SandraJD2@users.noreply.github.com>
2024-11-26 00:03:59 +0100

    feat: configure MongoDB connection with Atlas

--- app.js
+++ app.js
@@ -2,19 +2,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+require('dotenv').config();
+// Mongo DB
+const mongoose = require('mongoose');
+ 
 var catalogueRouter = require('./routes/catalogue');
-
+ 
 var app = express();
-
+ 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+ 
 app.use('/api/v1/catalogue', catalogueRouter);
-
+ 
+//Mongo DB setup
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_DATABASE}`);
+ 
+const db = mongoose.connection;
+ 
+db.on('error', console.error.bind(console, 'MongoDB Atlas connection error:'));
+db.once('open', function() {
+  console.log("Successfully connected to MongoDB Atlas");
+});
+ 
 module.exports = app;
